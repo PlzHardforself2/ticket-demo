fa8fd9134c3d6a4055ab4a5dcb232cbd505e4020
mingfeng.yi <Thekeeper-m>
2020-07-13 00:37:49 +0800

    update;

--- src/pages/shop/ticket/model.jsx
+++ src/pages/shop/ticket/model.jsx
@@ -2,11 +2,20 @@ const UserModel: UserModelType = {
   namespace: 'user',
   state: {},
   reducers: {
-      getList(state, action){
-        const data = {...}
+      getList(state, {payload}){  // action = > {type, payload}
+        // const data = {...} // 异步请求data转移到effect
+        return payload;
       }
   },
-  effects: {},
+  effects: {
+      *getRemote(action, effects) {  // effects = { put , call}
+          const data = {...}
+          yield  put({
+              type: 'getList',
+              payload: data
+          }) 
+      }
+  },
   subscriptions: {
     setup({ dispatch, history }) {
       return history.location(({ pathname }) => {
